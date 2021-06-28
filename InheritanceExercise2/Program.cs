decf0e5922ef50852250634c0f2f3ac9a82a801e
Huynhdavid888 <huynhdavid1245@gmail.com>
2021-06-27 22:33:29 -0700

    initital commit

---
+++ InheritanceExercise2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace InheritanceExercise2
+{
+    public class God
+    {
+        public static Human[] Create()
+        {
+            return new Human[] { new Man(), new Woman() };
+        }
+    }
+    public class Human
+    {
+
+    }
+    public class Man : Human
+    {
+
+    }
+    public class Woman : Human
+    {
+
+    }
+}
